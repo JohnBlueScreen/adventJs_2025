8d17e869e9a3f39801155b9a3b6a99acb2bb0126
JohnBlueScreen <johnbluescreen@gmail.com>
2025-12-03 09:26:24 +0100

    Ejercicio del día 2 resuelto con map

---
+++ 01/script.js
@@ -0,0 +1,81 @@
+/* Santa ha recibido una lista de regalos, pero algunos están defectuosos. Un regalo es defectuoso si su nombre contiene el carácter #.
+
+Ayuda a Santa escribiendo una función que reciba una lista de nombres de regalos y devuelva una nueva lista que solo contenga los regalos sin defectos. */
+
+/* const gifts1 = ['car', 'doll#arm', 'ball', '#train']
+const good1 = filterGifts(gifts1)
+console.log(good1)
+// ['car', 'ball'] */
+
+
+const gifts1 = ['car', 'doll#arm', 'ball', '#train'];
+
+// 1. Bucle for sencillo 
+const filterGifts = (arr) =>{
+
+    const gifts1Sinregalo =[];
+    for (let i = 0; i < arr.length; i++) {
+    
+        let palabra = arr[i];
+        let contieneCaracter = false;
+
+        for(let j = 0 ; j<palabra.length ; j++){
+            letra = palabra[j];
+            if(letra ==='#'){
+                contieneCaracter = true;
+                break;
+            }           
+        }
+        if(!contieneCaracter){
+            gifts1Sinregalo.push(palabra);
+            
+        }
+        
+    }
+    return gifts1Sinregalo;
+    
+}
+
+// 2. Solucion con index of
+
+const gifts2 = ['#broken', 'rusty','excelent','#rusteeze'];
+
+const filterGifts2 =(arr2) =>{
+
+    const giftsSinReagalo2 =[];
+    for (let i = 0; i < arr2.length; i++) {
+        if (arr2[i].indexOf('#') === -1) {
+            giftsSinReagalo2.push(arr2[i])
+        }       
+    }
+    return giftsSinReagalo2;
+
+}
+
+// 3. Solucion For of
+
+const gifts3 = ['car', 'doll#arm', 'ball', '#train'];
+
+const filterGifts3 =(arr3) =>{
+
+const gifts1Sinregalo3 = []
+
+for (const e of arr3) {
+    if(!e.includes('#')){
+        filterGifts3.push(e)
+    }
+}
+return gifts1Sinregalo3;
+}
+
+
+// Resultados
+
+const good1 = `Solucion 1 : Bucle for sencillo  \n` +  filterGifts(gifts1) + `\n` ;
+console.log(good1);
+
+const good2 = `Solucion 2 : Usando IndexOf ("#")  \n` +  filterGifts2(gifts2) + `\n`;
+console.log(good2)
+
+const good3 = `Solucion 2 : Usando for...of ("#")  \n` +  filterGifts2(gifts3) + `\n`;
+console.log(good3)
